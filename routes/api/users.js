d1f376c1dfd152e0cbf6fc0219b11bfae55560b9
LegendWorrier <148674491+LegendWorrier@users.noreply.github.com>
2023-11-07 13:41:37 +0900

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,8 +10,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
+// const jwt = require("jsonwebtoken");
+// const keys = require("../../config/keys");
 const passport = require("passport");
 //Load Input Validation
 const validateRegisterInput = require("../../validation/register");
