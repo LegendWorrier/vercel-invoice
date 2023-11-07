3746284ca3a6f2e1500e0b944b7e0cdc7f8ffec8
LegendWorrier <148674491+LegendWorrier@users.noreply.github.com>
2023-11-07 13:42:13 +0900

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -15,13 +15,13 @@ const bcrypt = require("bcryptjs");
 const passport = require("passport");
 //Load Input Validation
 const validateRegisterInput = require("../../validation/register");
-const validateLoginInput = require("../../validation/login");
+// const validateLoginInput = require("../../validation/login");
 
 //Load User model
 const User = require("../../models/User");
 const InvoiceData = require("../../models/InvoiceData");
 const ClientProfile = require("../../models/clientProfile");
-const clientProfile = require("../../models/clientProfile");
+// const clientProfile = require("../../models/clientProfile");
 const Download = require("../../models/Download")
 
 
